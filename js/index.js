eb82fa1a0e5b33d825323b752087b3964ada18ff
guac <123139032+guaccdev@users.noreply.github.com>
2023-12-21 10:35:06 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -69,6 +69,17 @@ function openURL(url) {
     window.navigator.serviceWorker
     .register("./uv.js", {
       scope: __uv$config.prefix,
+        form.addEventListener("submit", async (event) => {
+    event.preventDefault();
+    let url = input.value.trim();
+    console.log("URL:", url);
+    openURL(url);
+});
+
+function openURL(url) {
+    console.log("Opening URL:", url);
+}
+
     })
     .then(() => {
       if (!isUrl(url)) url = getSearchEngineURL() + url;
